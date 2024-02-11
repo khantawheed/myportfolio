47ebc0e147aaa5b187e9e758b49a39eaf9bfe955
Tawheed Khan <56296030+khantawheed@users.noreply.github.com>
2024-02-11 07:45:12 -0800

    small changes

--- src/app/details-musicapp/page.js
+++ src/app/details-musicapp/page.js
@@ -3,7 +3,7 @@ import DetailsComponent from "@/component/DetailsComponent"
 const DetailPage = () => {
   return (
     <div>
-      <DetailsComponent title="Music App" imgsrc='/image/musicapp.png' code='https://github.com/khantawheed/musicapp2'
+      <DetailsComponent title="Music App" imgsrc='/image/musicapp.png' code='https://github.com/khantawheed/musicapp2' live='https://musicapp12.netlify.app'
       description="TunesHub is a React.js and Tailwind CSS-powered music app that brings the latest chart-toppers and releases to your fingertips. With React Redux for seamless state management and Rapid API integration, it delivers real-time updates for a dynamic user experience. Dive into top charts, discover new tracks, and create personalized playlists—all in a sleek and user-friendly interface. Immerse yourself in the world of music with TunesHub, where React.js meets top-notch design for an unparalleled listening journey."/>
     </div>
   )
--- src/component/ProjectsComponent.js
+++ src/component/ProjectsComponent.js
@@ -6,7 +6,7 @@ const ProjectsComponent = () => {
     <div className='min-h-screen flex flex-col justify-center lg:px-32 px-5 bg-[#081b29]'>
       <h1 className='font-semibold text-center text-4xl mt-16 mb-8 text-sky-500'>Projects</h1>
       <div className='flex flex-wrap pb-8 gap-4 justify-center lg:flex-row'>
-        <ProjectCard title="Music App" img='/image/musicapp.png' code='https://github.com/khantawheed/musicapp2' detail='/details-musicapp'/>
+        <ProjectCard title="Music App" img='/image/musicapp.png' code='https://github.com/khantawheed/musicapp2' live='https://musicapp12.netlify.app' detail='/details-musicapp'/>
         <ProjectCard title="E-commers" img='/image/bikeee.png' code='https://github.com/khantawheed/bikee' live='https://bikeee-rho.vercel.app/' detail='/details-ecommers'/>
         <ProjectCard title="Cafe website" img='/image/coffee site.png' code='https://github.com/khantawheed/cafecoffee' live='https://main--shimmering-pastelito-688b70.netlify.app/' detail='/details-cafe'/>
         <ProjectCard title="Blockchain Based Data Transfer" img='/image/blockchain.png' code='https://github.com/khantawheed/blockchain_project' detail='/details-blockchain'/>
